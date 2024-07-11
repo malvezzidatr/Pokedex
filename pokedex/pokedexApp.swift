4f20d070690da88cf6a935d3be58891c05a83f48
caio malvezzi <caio.malvezzi@compasso.com.br>
2024-07-11 15:51:28 -0300

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Caio Malvezzi on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
